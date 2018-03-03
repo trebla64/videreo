12a7ee82e74c417ae529784e36c4377778ae0ee1
Albert Minnie <albert.minnie@gmail.com>
2018-03-03 15:58:39 +0200

    Code for loading PLY file into memory

--- src/ply_loader.cpp
+++ src/ply_loader.cpp
@@ -3,5 +3,21 @@
 
 bool PLYLoader::LoadFromFile(const char *file)
 {
+	FILE *fp = fopen(file, "rb");
+	if (fp) {
+		fseek(fp, 0, SEEK_END);
+		unsigned size = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+		auto data = std::make_unique<uint8_t[]>(size);
+		if (data.get()) {
+			fread(data.get(), 1, size, fp);
+			fclose(fp);
+		}
+		else {
+			fclose(fp);
+			return false;
+		}
+	}
+
 	return true;
 }
